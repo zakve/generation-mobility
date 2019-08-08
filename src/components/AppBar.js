ffe3d13a931a1feb4ce86e9de5c2f72538fe7445
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-08-08 21:48:07 +0200

    Update AppBar with links

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,32 +1,32 @@
 import React from 'react'
 import Toolbar from '@material-ui/core/Toolbar';
-import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
-
 function AppBar() {
     return (
-        <Toolbar className="app-bar">
+        <Toolbar className="app-bar" disableGutters="true">
             <Typography variant="h6" color="inherit" className="logo">
                 Martin Zaklasnik
             </Typography>
             <IconButton
-                aria-label="account of current user"
+                aria-label="Linkedin link"
                 aria-controls="menu-appbar"
                 aria-haspopup="true"
                 color="inherit"
+                onClick={() => window.open("https://www.linkedin.com/in/zaklasnikm/", "_blank")}
             >
                 <FontAwesomeIcon icon={faLinkedin} />
             </IconButton>
             <IconButton
-                aria-label="account of current user"
+                aria-label="Github link"
                 aria-controls="menu-appbar"
                 aria-haspopup="true"
                 color="inherit"
+                onClick={() => window.open("https://github.com/zakve", "_blank")}
             >
                 <FontAwesomeIcon icon={faGithub} />
             </IconButton>
