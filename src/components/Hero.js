9eee4a2d4d15abb331a299f1745673d9abd65d03
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-08-09 15:32:17 +0200

    Add title underline and arrow down

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import AppBar from './AppBar';
 
+import ArrowDownIcon from '@material-ui/icons/ArrowDownward';
+
 function Hero() {
     return (
         <div className="hero-image">
@@ -10,7 +12,9 @@ function Hero() {
                 <Typography color="inherit" align="center" variant="h2" marked="center">
                     My stay in Netherlands
                 </Typography>
+                <p className="title-underline"></p>
             </div>
+            <ArrowDownIcon className="arrow-down" />
         </div>
     )
 }
