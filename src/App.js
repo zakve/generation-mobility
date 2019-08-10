2c6f158c93f41ca6078a01026d1ee5cf7e12ecc7
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-08-10 17:12:19 +0200

    Add more content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import Quote from './components/Quote'
 import TableContent from './components/TableContent'
 import ContentCard from "./components/ContentCard";
+import ContentCard2 from "./components/ContentCard2";
 import AppFooter from "./components/AppFooter";
 import './assets/css/App.css';
 
@@ -12,6 +13,10 @@ function App() {
       <Hero />
       <Quote />
       <TableContent />
+      <ContentCard />
+      <ContentCard2 />
+      <ContentCard />
+      <ContentCard2 />
       <ContentCard />
       <AppFooter />
     </div>
---
+++ src/components/ContentCard2.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography';
+import { Container } from '@material-ui/core';
+
+export default function ContentCard() {
+    return (
+        <div className="content-card-white">
+            <Container>
+                <div className="hero-text">
+                    <p className="title-uperline"></p>
+                    <Typography id="title1" color="inherit" align="left" variant="h5">
+                        Before flight
+                    </Typography>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque. Pellentesque ipsum. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Aliquam erat volutpat. Etiam posuere lacus quis dolor. Fusce aliquam vestibulum ipsum. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Integer vulputate sem a nibh rutrum consequat. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Proin in tellus sit amet nibh dignissim sagittis. Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Suspendisse sagittis ultrices augue. Vivamus porttitor turpis ac leo. Nulla quis diam.
+                </p>
+            </Container>
+        </div>
+    )
+}
