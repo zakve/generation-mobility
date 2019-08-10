b6818c94ce5f57abaac55f9b2baa84b777d922fc
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-08-10 16:44:51 +0200

    Add first content

--- src/components/TableContent.js
+++ src/components/TableContent.js
@@ -91,6 +91,26 @@ function TableContent() {
                     </CardActionArea>
                 </Card>
             </Paper>
+
+            <Paper elevation={7} className="card">
+                <Card>
+                    <CardActionArea>
+                        <CardMedia
+                            className="card-img"
+                            image={Map}
+                            title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Why Netherlands?
+                        </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                I choosed Netherlands quite randomly
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </Paper>
         </div>
 
     )
