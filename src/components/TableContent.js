38c4b2ae1df4f186d218fbff51f2aa898511b519
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-08-11 17:43:40 +0200

    Add card links

--- src/components/TableContent.js
+++ src/components/TableContent.js
@@ -5,7 +5,6 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 
-import Map from "../assets/img/map.jpg";
 import { Paper } from '@material-ui/core';
 
 
@@ -14,101 +13,111 @@ function TableContent() {
         <div className="table-contents">
             <Paper elevation={7} className="card">
                 <Card>
-                    <CardActionArea>
-                        <CardMedia
-                            className="card-img"
-                            image="https://images.unsplash.com/photo-1528918387630-6b575e9fe5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1967&q=80"
-                            title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Why Netherlands?
+                    <a href='#why-netherlands'>
+                        <CardActionArea>
+                            <CardMedia
+                                className="card-img"
+                                image="https://images.unsplash.com/photo-1528918387630-6b575e9fe5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1967&q=80"
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Why Netherlands?
                         </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                I choosed Netherlands quite randomly
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    I choosed Netherlands quite randomly
                         </Typography>
-                        </CardContent>
-                    </CardActionArea>
+                            </CardContent>
+                        </CardActionArea>
+                    </a>
                 </Card>
             </Paper>
 
             <Paper elevation={7} className="card">
                 <Card>
-                    <CardActionArea>
-                        <CardMedia
-                            className="card-img"
-                            image="https://images.unsplash.com/photo-1509650926597-25eead3b6ca9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                            title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Before flight
+                    <a href='#before-flight'>
+                        <CardActionArea>
+                            <CardMedia
+                                className="card-img"
+                                image="https://images.unsplash.com/photo-1509650926597-25eead3b6ca9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Before flight
                         </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                I choosed Netherlands quite randomly
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    I choosed Netherlands quite randomly
                         </Typography>
-                        </CardContent>
-                    </CardActionArea>
+                            </CardContent>
+                        </CardActionArea>
+                    </a>
                 </Card>
             </Paper>
 
             <Paper elevation={7} className="card">
                 <Card>
-                    <CardActionArea>
-                        <CardMedia
-                            className="card-img"
-                            image="https://images.unsplash.com/photo-1542296332-2e4473faf563?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                            title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Arrival
+                    <a href='#arrival'>
+                        <CardActionArea>
+                            <CardMedia
+                                className="card-img"
+                                image="https://images.unsplash.com/photo-1542296332-2e4473faf563?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Arrival
                         </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                I choosed Netherlands quite randomly
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    I choosed Netherlands quite randomly
                         </Typography>
-                        </CardContent>
-                    </CardActionArea>
+                            </CardContent>
+                        </CardActionArea>
+                    </a>
                 </Card>
             </Paper>
 
             <Paper elevation={7} className="card">
                 <Card>
-                    <CardActionArea>
-                        <CardMedia
-                            className="card-img"
-                            image="https://images.unsplash.com/photo-1471560090527-d1af5e4e6eb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-                            title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                After return
+                    <a href='#after-return'>
+                        <CardActionArea>
+                            <CardMedia
+                                className="card-img"
+                                image="https://images.unsplash.com/photo-1471560090527-d1af5e4e6eb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    After return
                         </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                I choosed Netherlands quite randomly
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    I choosed Netherlands quite randomly
                         </Typography>
-                        </CardContent>
-                    </CardActionArea>
+                            </CardContent>
+                        </CardActionArea>
+                    </a>
                 </Card>
             </Paper>
 
             <Paper elevation={7} className="card">
                 <Card>
-                    <CardActionArea>
-                        <CardMedia
-                            className="card-img"
-                            image="https://images.unsplash.com/photo-1462642109801-4ac2971a3a51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1266&q=80"
-                            title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Summary
+                    <a href='#summary'>
+                        <CardActionArea>
+                            <CardMedia
+                                className="card-img"
+                                image="https://images.unsplash.com/photo-1462642109801-4ac2971a3a51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1266&q=80"
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Summary
                         </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                I choosed Netherlands quite randomly
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    I choosed Netherlands quite randomly
                         </Typography>
-                        </CardContent>
-                    </CardActionArea>
+                            </CardContent>
+                        </CardActionArea>
+                    </a>
                 </Card>
             </Paper>
         </div>
