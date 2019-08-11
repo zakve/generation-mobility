a1b667b6a06605baf86cd0f09364ac88a6778635
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-08-11 22:43:57 +0200

    Add content

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,7 +5,7 @@ export default function AppFooter() {
     return (
         <div className="app-footer">
             <Container>
-                This landing page is created using <a href="https://reactjs.org/">React</a> and <a href="https://material-ui.com/">Material design</a> and images are from <a href="https://unsplash.com/">Unsplash</a>. Using React for landing pages is not an ideal solution because it is dynamically loaded content that is difficult to read for search robots with resulting in poor SEO (Search Engine Optimization). The main reason for the use of react in this project is to learn and try new technology. All source codes are available at <a href="https://github.com/zakve">Github</a>.
+                This landing page is created using <a href="https://reactjs.org/">React</a>, <a href="https://material-ui.com/">Material design</a> and images from <a href="https://unsplash.com/">Unsplash</a>. Using React for landing pages is not an ideal solution because the page content is dynamically loaded and that is difficult to read for search robots with resulting in poor SEO (Search Engine Optimization). The main reason for the use of React in this project is to learn and try new technology. All source codes are available on <a href="https://github.com/zakve">Github</a>.
             </Container>
         </div>
     )
