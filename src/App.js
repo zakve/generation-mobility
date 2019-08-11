636e6767b6914c8c30496f102fcdf49f19b0b95e
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-08-11 18:14:02 +0200

    Add content and styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Hero from './components/Hero'
 import Quote from './components/Quote'
 import TableContent from './components/TableContent'
 import ContentCard from "./components/ContentCard";
-import ContentCard2 from "./components/ContentCard2";
 import AppFooter from "./components/AppFooter";
 import './assets/css/App.css';
 
@@ -13,11 +12,11 @@ function App() {
       <Hero />
       <Quote />
       <TableContent />
-      <ContentCard />
-      <ContentCard2 />
-      <ContentCard />
-      <ContentCard2 />
-      <ContentCard />
+      <ContentCard id="why-netherlands" title="Why Netherlands?" text="As a Master's degree student I had opportunity for a six-month exchange stay in another country through Erasmus+ program. I was choosing between Portugal and the Netherlands. On the Googl I was looking for information about every country and because I love cycling, Netherlands was the number 1 choice for me." />
+      <ContentCard id="before-flight" title="Before flight" text="Paperwork at school, with the other university, financial contract, air tickets, train tickets, accommodation, what to take, not far but for me the first flight in my life, insurance" />
+      <ContentCard id="arrival" title="Arrival" text="" />
+      <ContentCard id="after-return" title="After return" text="" />
+      <ContentCard id="summary" title="Summary" text="" />
       <AppFooter />
     </div>
   );
--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -2,18 +2,18 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import { Container } from '@material-ui/core';
 
-export default function ContentCard() {
+export default function ContentCard(props) {
     return (
-        <div className="content-card">
+        <div id={props.id} className="content-card">
             <Container>
                 <div className="hero-text">
                     <p className="title-uperline"></p>
-                    <Typography id="title1" color="inherit" align="left" variant="h5">
-                        Why Netherlands?
+                    <Typography align="left" variant="h5">
+                        {props.title}
                     </Typography>
                 </div>
                 <p>
-                    As a Master's degree student I had opportunity for a six-month exchange stay in another country through Erasmus+ program. I was choosing between Portugal and the Netherlands. On the Googl I was looking for information about every country and because I love cycling, Netherlands was the number 1 choice for me.
+                    {props.text}
                 </p>
             </Container>
         </div>
